e34b3b6003ed3afaf43e899dd9f87ec0ba2aaac4
linamoussadek <lmous090@uottawa.ca>
2023-03-30 03:28:07 -0400

    Updates

---
+++ precache-manifest.d69a7b55c6f63a1af138a0254253bf73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f048b3110e18c4cb6fd4",
+    "url": "/static/css/main.6beb71f1.chunk.css"
+  },
+  {
+    "revision": "f048b3110e18c4cb6fd4",
+    "url": "/static/js/main.f048b311.chunk.js"
+  },
+  {
+    "revision": "ed27882ff9ec9177fee6",
+    "url": "/static/js/1.ed27882f.chunk.js"
+  },
+  {
+    "revision": "aeded5a948b84ab22fea",
+    "url": "/static/css/2.cf97b532.chunk.css"
+  },
+  {
+    "revision": "aeded5a948b84ab22fea",
+    "url": "/static/js/2.aeded5a9.chunk.js"
+  },
+  {
+    "revision": "cf50cacd8b08b791f7a1",
+    "url": "/static/js/runtime~main.cf50cacd.js"
+  },
+  {
+    "revision": "b353d87c189ffa42a93474b478b1bb30",
+    "url": "/index.html"
+  }
+];
